1483d985c6f8ebcfafd8fd5f61b61e781475df6a
pokemium <bluejapan73+dev@gmail.com>
2021-12-16 21:06:06 +0900

    fix signature

--- blip.go
+++ blip.go
@@ -7,8 +7,8 @@ import "unsafe"
 type Blip C.struct_blip_t
 
 // blip_new
-func New(size int) *Blip {
-	b := C.blip_new(C.int(size))
+func New(size uint) *Blip {
+	b := C.blip_new(C.int(int(size)))
 	return (*Blip)(b)
 }
 
@@ -20,8 +20,9 @@ func (b *Blip) Delete() {
 }
 
 // blip_set_rates
-func (b *Blip) SetRates(clockRate, sampleRate float64) {
+func (b *Blip) SetRates(clockRate, sampleRate float64) error {
 	C.blip_set_rates((*C.struct_blip_t)(b), C.double(clockRate), C.double(sampleRate))
+	return nil
 }
 
 // blip_clear
@@ -30,14 +31,15 @@ func (b *Blip) Clear() {
 }
 
 // blip_clocks_needed
-func (b *Blip) ClocksNeeded(samples int) int {
-	result := C.blip_clocks_needed((*C.struct_blip_t)(b), C.int(samples))
+func (b *Blip) ClocksNeeded(samples uint) int {
+	result := C.blip_clocks_needed((*C.struct_blip_t)(b), C.int(int(samples)))
 	return int(result)
 }
 
 // blip_end_frame
-func (b *Blip) EndFrame(t uint) {
+func (b *Blip) EndFrame(t uint) error {
 	C.blip_end_frame((*C.struct_blip_t)(b), C.uint(t))
+	return nil
 }
 
 // blip_samples_avail
@@ -46,16 +48,22 @@ func (b *Blip) SamplesAvail() int {
 }
 
 // blip_read_samples
-func (b *Blip) ReadSamples(out unsafe.Pointer, count, stereo int) int {
-	return int(C.blip_read_samples((*C.struct_blip_t)(b), (*C.short)(out), C.int(count), C.int(stereo)))
+func (b *Blip) ReadSamples(out unsafe.Pointer, count int, stereo bool) int {
+	stereoI := 0
+	if stereo {
+		stereoI = 1
+	}
+	return int(C.blip_read_samples((*C.struct_blip_t)(b), (*C.short)(out), C.int(count), C.int(stereoI)))
 }
 
 // blip_add_delta
-func (b *Blip) AddDelta(time uint, delta int) {
+func (b *Blip) AddDelta(time uint, delta int) error {
 	C.blip_add_delta((*C.struct_blip_t)(b), C.uint(time), C.int(delta))
+	return nil
 }
 
 // blip_add_delta_fast
-func (b *Blip) AddDeltaFast(time uint, delta int) {
+func (b *Blip) AddDeltaFast(time uint, delta int) error {
 	C.blip_add_delta_fast((*C.struct_blip_t)(b), C.uint(time), C.int(delta))
+	return nil
 }
